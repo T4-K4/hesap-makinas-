50ef754de3ed565484cd85e9c8e8cde756272800
TALHA KARADAŞ <m.talhakaradas06@gmail.com>
2025-03-15 12:54:20 +0300

    üsteki tag silindi

--- lib/hesap_makina.dart
+++ lib/hesap_makina.dart
@@ -79,6 +79,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: const Color.fromARGB(19, 83, 173, 185),
